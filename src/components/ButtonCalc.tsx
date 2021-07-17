663730ea360e93e7a7692200f89035e290422d73
Amaury Soto <asoto@addi.com>
2021-07-17 14:52:05 -0500

    Others button, add TochableOpacity

--- src/components/ButtonCalc.tsx
+++ src/components/ButtonCalc.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { styles } from '../theme/appTheme'
+import React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {styles} from '../theme/appTheme';
 
-
-interface Props{
-    text:string,
-    color?:string
+interface Props {
+  text: string;
+  color?: string;
+  widthButton?: boolean
 }
 
-export const ButtonCalc = ({text, color='#2d2d2d'}: Props ) => {
-    return (
-     <View 
-      style={
-          {...styles.button,
-            backgroundColor:color
-        
-        }
-          }> 
-        <Text style={styles.buttonText}>{text}</Text>
-     </View>
-    )
-}
+export const ButtonCalc = ({text, color = '#2d2d2d', widthButton=false}: Props) => {
+  return (
+
+    <TouchableOpacity>
+        <View style={{
+            ...styles.button, 
+            backgroundColor: color,
+            width: (widthButton) ? 180 : 80
+            }}>
+        <Text
+            style={{
+            ...styles.buttonText,
+            color: color === '#9b9b9b' ? 'black' : 'white',
+            }}>
+            {text}
+        </Text>
+        </View>
+    </TouchableOpacity>
+
+  );
+};
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -17,6 +17,34 @@ export const CalculatorScreen = () => {
                 <ButtonCalc text='del' color='#9b9b9b'/>
                 <ButtonCalc text='/' color='#ff9427'/>
             </View>
+            <View style={styles.row}>
+                {/* Buttons */}
+                <ButtonCalc text='7' />
+                <ButtonCalc text='8'  />
+                <ButtonCalc text='9' />
+                <ButtonCalc text='X' color='#ff9427'/>
+            </View>
+            <View style={styles.row}>
+                {/* Buttons */}
+                <ButtonCalc text='4' />
+                <ButtonCalc text='5'  />
+                <ButtonCalc text='6' />
+                <ButtonCalc text='-' color='#ff9427'/>
+            </View>
+            <View style={styles.row}>
+                {/* Buttons */}
+                <ButtonCalc text='1' />
+                <ButtonCalc text='2'  />
+                <ButtonCalc text='3' />
+                <ButtonCalc text='+' color='#ff9427'/>
+            </View>
+            <View style={styles.row}>
+                {/* Buttons */}
+                <ButtonCalc text='0' widthButton />
+                <ButtonCalc text='.'  />
+                <ButtonCalc text='=' color='#ff9427'/>
+            </View>
+
 
 
         </View>
